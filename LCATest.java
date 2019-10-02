3e9af2c0d06645d36c8b4f1494d844ec586107ae
ciaraos <47390332+ciaraos@users.noreply.github.com>
2019-10-02 12:47:29 +0100

    Added tests to be completed, none passing

---
+++ LCATest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LCATest {
+
+	@Test
+	public void testConstructor() {
+		
+	}
+
+	@Test 
+	public void testFindLCA() {
+		
+	}
+	
+	@Test 
+	public void testFindPathTrue() {
+		
+	}
+	
+	@Test
+	public void testFindPathFalse() {
+		
+	}
+}
