f0f2e2efa3ea31007505039ebf7c5ce92c50dba6
ciaraos <47390332+ciaraos@users.noreply.github.com>
2019-10-03 17:47:36 +0100

    Testing if correct LCA value is found: passed

--- LCATest.java
+++ LCATest.java
@@ -1,26 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class LCATest {
+public class LCATest {
 
 	@Test
-	public void testConstructor() {
-		
-	}
-
-	@Test 
-	public void testFindLCA() {
-		
-	}
-	
-	@Test 
-	public void testFindPathTrue() {
-		
-	}
-	
-	@Test
-	public void testFindPathFalse() {
+	public void testLCA() {
+		LowestCommonAncestor testTree = new LowestCommonAncestor();
+		assertNull("Testing empty tree", testTree.LCAroot);
+		testTree.LCAroot = new Node(3);
+		testTree.LCAroot.leftNode = new Node(4);
+		testTree.LCAroot.rightNode = new Node(5);
+		assertEquals("Data value is correct:",3,testTree.LCAroot.data);
+		assertEquals("Data value is correct:",4,testTree.LCAroot.leftNode.data);
+		assertEquals("Data value is correct:",5,testTree.LCAroot.rightNode.data);
 		
 	}
 }
