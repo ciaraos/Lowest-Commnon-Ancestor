4ce6c7dd7d7b90d191a40d2929c7826d36281e38
ciaraos <47390332+ciaraos@users.noreply.github.com>
2019-10-04 19:38:32 +0100

    Test for empty binary tree: passed

--- LCATest.java
+++ LCATest.java
@@ -5,14 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class LCATest {
-	
-	//test pass
-	//test fail
-	//test one node
-	//test two nodes
 
 	@Test
-	public void testEmpty() {
+	public void testEmptyTree() {
 		LowestCommonAncestor testTree = new LowestCommonAncestor();
 		assertNull("Testing empty tree", testTree.root);
 	}
@@ -35,13 +30,12 @@ class LCATest {
 	@Test
 	public void testLCA() {
 		LowestCommonAncestor testTree = new LowestCommonAncestor();
-		testTree.root = new Node(3);
-		testTree.root.left = new Node(4);
-		testTree.root.right = new Node(5);
-		assertEquals("Data value is correct:",3,testTree.root.data);
-		assertEquals("Data value is correct:",4,testTree.root.left.data);
-		assertEquals("Data value is correct:",5,testTree.root.right.data);
-		
+		testTree.root = new Node(1);
+		testTree.root.left = new Node(2);
+		testTree.root.right = new Node(3);
+		assertEquals("Data value is correct:",1,testTree.root.data);
+		assertEquals("Data value is correct:",2,testTree.root.left.data);
+		assertEquals("Data value is correct:",3,testTree.root.right.data);
 	}
 	
 	@Test
@@ -55,15 +49,9 @@ class LCATest {
 		testTree.root.right.left = new Node(6); 
 		testTree.root.right.right = new Node(7); 
 
-		assertEquals("LCA(4,5) should be 2.",testTree.findLowestCommonAncestor(4,5),2);
-		assertEquals("LCA(4,6) should be 1.",testTree.findLowestCommonAncestor(4,6),1);
+		assertEquals("LCA(2,3) should be 1.",testTree.findLowestCommonAncestor(2,3),1);
+		assertEquals("LCA(3,6) should be 3.",testTree.findLowestCommonAncestor(3,6),3);
 		assertEquals("LCA(3,4) should be 1.",testTree.findLowestCommonAncestor(3,4),1);
 		assertEquals("LCA(2,4) should be 2.",testTree.findLowestCommonAncestor(2,4),2);
-		assertEquals("LCA of non existent node",testTree.findLowestCommonAncestor(8, 7),-1);
-		
-		testTree.root.left = new Node(5);
-		assertEquals("tree with no root.",testTree.findLowestCommonAncestor(5, 0),-1);
-		  
 	}
 }
-
